685af264caf9cc30c46d9c256f84b72910409bca
yeonghui Hwang <nsun14363@gmail.com>
2024-09-24 14:19:07 +0900

    Delete 프로그래머스/1 directory

--- 프로그래머스/1/12928. 약수의 합/약수의 합.py
+++
@@ -1,6 +0,0 @@
-def solution(n):
-    sum = 0
-    for i in range(1, n+1):
-        if n%i==0: sum += i
-        
-    return sum
--- 프로그래머스/1/12944. 평균 구하기/평균 구하기.py
+++
@@ -1,6 +0,0 @@
-def solution(arr):
-    sum = 0
-    for i in range(len(arr)):
-        sum += arr[i]
-    
-    return sum/len(arr)
